701826e1e80d2656d01ba6fb667c8921d804b1e6
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:22:50 +0200

    add `shadowColor` to `ManagerColors` class

--- lib/core/resource/manager_colors.dart
+++ lib/core/resource/manager_colors.dart
@@ -5,5 +5,5 @@ class ManagerColors {
   static const Color secondaryColor = Color(0xfffa7d22);
   static const Color blackColor = Color(0xff010e0a);
   static const Color whiteColor = Color(0xfffffff2);
-
+  static Color shadowColor = blackColor.withOpacity(0.25);
 }
