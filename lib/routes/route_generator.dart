d84f69b2934f07bc65dbfa11f96b38a953b2ba87
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:58:26 +0200

    add `loginScreen` route to `Routes` and `RouteGenerator`

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:wafir_mobile/core/resource/manager_colors.dart';
 import 'package:wafir_mobile/core/resource/manager_fonts.dart';
 import 'package:wafir_mobile/core/resource/manager_strings.dart';
+import 'package:wafir_mobile/features/login/presentation/view/screens/login_screen.dart';
 import 'package:wafir_mobile/features/on_boarding/presentation/view/screens/on_boarding_screen.dart';
 import 'package:wafir_mobile/routes/routes.dart';
 
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings setting) {
     switch (setting.name) {
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       default:
