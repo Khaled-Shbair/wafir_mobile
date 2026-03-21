1f25fa6f1c68bc4f19e916feb10190dbfd6029ad
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:23:46 +0200

    implement `OnBoardingScreen` with `PageView` and custom content widgets

---
+++ lib/features/on_boarding/presentation/view/screens/on_boarding_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:wafir_mobile/core/resource/manager_assets.dart';
+import 'package:wafir_mobile/core/resource/manager_strings.dart';
+import 'package:wafir_mobile/features/on_boarding/presentation/view/widgets/custom_page_view_content.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: 0,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'وافر',
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          CustomPageViewContent(
+            image: ManagerAssets.onBoardingImageOne,
+            title: ManagerStrings.titleOnBoardingOne,
+            subTitle: ManagerStrings.subTitleOnBoardingOne,
+            textTopButton: ManagerStrings.next,
+            textBottomButton: ManagerStrings.skip,
+            functionTopButton: () {
+              _pageController.nextPage(
+                duration: const Duration(milliseconds: 800),
+                curve: Curves.easeIn,
+              );
+            },
+            functionBottomButton: () {
+              _pageController.animateToPage(
+                2,
+                duration: const Duration(milliseconds: 800),
+                curve: Curves.easeIn,
+              );
+            },
+          ),
+          CustomPageViewContent(
+            image: ManagerAssets.onBoardingImageTwo,
+            title: ManagerStrings.titleOnBoardingTwo,
+            subTitle: ManagerStrings.subTitleOnBoardingTwo,
+            textTopButton: ManagerStrings.next,
+            textBottomButton: ManagerStrings.previous,
+            functionTopButton: () {
+              _pageController.nextPage(
+                duration: const Duration(milliseconds: 800),
+                curve: Curves.easeIn,
+              );
+            },
+            functionBottomButton: () {
+              _pageController.previousPage(
+                duration: const Duration(milliseconds: 800),
+                curve: Curves.easeOut,
+              );
+            },
+          ),
+          CustomPageViewContent(
+            image: ManagerAssets.onBoardingImageThree,
+            title: ManagerStrings.titleOnBoardingThree,
+            subTitle: ManagerStrings.subTitleOnBoardingThree,
+            textTopButton: ManagerStrings.login,
+            textBottomButton: ManagerStrings.loginAsGuest,
+            functionTopButton: () {},
+            functionBottomButton: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
