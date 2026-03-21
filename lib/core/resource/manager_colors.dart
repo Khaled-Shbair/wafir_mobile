ee8101ded172e4551acf15e12786758091612afe
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:13:27 +0200

    add `ManagerColors` class with core application color constants

---
+++ lib/core/resource/manager_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ManagerColors {
+  static const Color primaryColor = Color(0xff00845E);
+  static const Color secondaryColor = Color(0xfffa7d22);
+  static const Color blackColor = Color(0xff010e0a);
+  static const Color whiteColor = Color(0xfffffff2);
+
+}
