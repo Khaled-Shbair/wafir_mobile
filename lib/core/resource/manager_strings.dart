9c8f4669f92f909c77d15fd4496c63a0e547c17a
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:13:53 +0200

    add `ManagerStrings` class with localized string constants

---
+++ lib/core/resource/manager_strings.dart
@@ -0,0 +1,16 @@
+import 'package:easy_localization/easy_localization.dart';
+
+class ManagerStrings {
+  static String noRouteFound = 'no_route_found'.tr();
+  static String titleOnBoardingOne = 'title_on_boarding_one'.tr();
+  static String titleOnBoardingTwo = 'title_on_boarding_two'.tr();
+  static String titleOnBoardingThree = 'title_on_boarding_three'.tr();
+  static String subTitleOnBoardingOne = 'sub_title_on_boarding_one'.tr();
+  static String subTitleOnBoardingTwo = 'sub_title_on_boarding_two'.tr();
+  static String subTitleOnBoardingThree = 'sub_title_on_boarding_three'.tr();
+  static String login = 'login'.tr();
+  static String loginAsGuest = 'login_as_guest'.tr();
+  static String next = 'next'.tr();
+  static String previous = 'previous'.tr();
+  static String skip = 'skip'.tr();
+}
