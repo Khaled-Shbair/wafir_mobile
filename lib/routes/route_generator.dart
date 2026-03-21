b4591bba5dff481ff30165eb3f7eb1852af83ec4
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:21:37 +0200

    add `RouteGenerator` and `Routes` classes for navigation management

---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:wafir_mobile/core/resource/manager_colors.dart';
+import 'package:wafir_mobile/core/resource/manager_fonts.dart';
+import 'package:wafir_mobile/core/resource/manager_strings.dart';
+import 'package:wafir_mobile/features/on_boarding/presentation/view/screens/on_boarding_screen.dart';
+import 'package:wafir_mobile/routes/routes.dart';
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings setting) {
+    switch (setting.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            ManagerStrings.noRouteFound,
+            style: TextStyle(
+              color: ManagerColors.blackColor,
+              fontSize: ManagerFontsSizes.f24,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Text(
+            ManagerStrings.noRouteFound,
+            style: TextStyle(
+              color: ManagerColors.blackColor,
+              fontSize: ManagerFontsSizes.f24,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,3 @@
+class Routes{
+  static const String onBoardingScreen='/on_boarding_screen';
+}
