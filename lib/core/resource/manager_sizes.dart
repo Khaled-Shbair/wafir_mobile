708016fa0970dc7f72d0ed24fdce685789397d10
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:13:43 +0200

    add `ManagerWidths`, `ManagerHeights`, and `ManagerRadius` classes with responsive size constants

---
+++ lib/core/resource/manager_sizes.dart
@@ -0,0 +1,22 @@
+import 'package:wafir_mobile/core/size_util/size_util.dart';
+
+class ManagerWidths {
+  static const double infinity = double.infinity;
+  static  double w20 = SizeUtil.setWidth(20);
+
+}
+
+class ManagerHeights {
+  static const double infinity = double.infinity;
+  static double h10 = SizeUtil.setHeight(10);
+  static double h50 = SizeUtil.setHeight(50);
+  static double h20 = SizeUtil.setHeight(20);
+  static double h30 = SizeUtil.setHeight(30);
+  static double h40 = SizeUtil.setHeight(40);
+  static double h350 = SizeUtil.setHeight(350);
+
+}
+
+class ManagerRadius {
+  static double r45 = SizeUtil.setRadius(45);
+}
