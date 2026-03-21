33a17a7f9958d5f68b239dac9db243cd5b8abb9a
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:14:15 +0200

    add `ManagerFontsSizes`, `ManagerFontWeight`, and `ManagerFontFamily` classes with font-related constants and responsive sizes

---
+++ lib/core/resource/manager_fonts.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:wafir_mobile/core/size_util/size_util.dart';
+
+class ManagerFontsSizes {
+  static double f16 = SizeUtil.setFontSize(16);
+  static double f18 = SizeUtil.setFontSize(18);
+  static double f22 = SizeUtil.setFontSize(22);
+
+  static double f24 = SizeUtil.setFontSize(24);
+}
+
+class ManagerFontWeight {
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+  static const FontWeight extraBold = FontWeight.w800;
+}
+
+class ManagerFontFamily {
+  static const String tajawal = 'Tajawal';
+}
