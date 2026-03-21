4efa89645ee950818db9ab42cff3a6e576b8a02a
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-21 09:22:41 +0200

    add `ManagerAssets` class with onboarding image and language asset constants

---
+++ lib/core/resource/manager_assets.dart
@@ -0,0 +1,7 @@
+class ManagerAssets{
+  static const String onBoardingImageOne = 'assets/images/on_boarding_images/on_boarding_image_one.png';
+  static const String onBoardingImageTwo = 'assets/images/on_boarding_images/on_boarding_image_two.png';
+  static const String onBoardingImageThree = 'assets/images/on_boarding_images/on_boarding_image_three.png';
+
+  static const String languages = 'assets/languages';
+}
