8bef786c59cd3b7821d133213eae447cf4866fb5
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-23 21:13:54 +0200

    add: add Google icon asset for login screen

--- lib/core/resource/manager_assets.dart
+++ lib/core/resource/manager_assets.dart
@@ -2,6 +2,7 @@ class ManagerAssets{
   static const String onBoardingImageOne = 'assets/images/on_boarding_images/on_boarding_image_one.png';
   static const String onBoardingImageTwo = 'assets/images/on_boarding_images/on_boarding_image_two.png';
   static const String onBoardingImageThree = 'assets/images/on_boarding_images/on_boarding_image_three.png';
+  static const String googleIcon = 'assets/images/login_images/google_Icon.png';
 
   static const String languages = 'assets/languages';
 }
