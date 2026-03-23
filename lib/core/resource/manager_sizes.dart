abf8835cd2521a1346154aa72b1e20692579642b
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-23 21:14:59 +0200

    add: introduce additional size constants for widths, radii, and icon sizes

--- lib/core/resource/manager_sizes.dart
+++ lib/core/resource/manager_sizes.dart
@@ -4,6 +4,7 @@ class ManagerWidths {
   static const double infinity = double.infinity;
   static  double w10 = SizeUtil.setWidth(10);
   static  double w20 = SizeUtil.setWidth(20);
+  static  double w5 = SizeUtil.setWidth(5);
 
 }
 
@@ -21,4 +22,12 @@ class ManagerHeights {
 
 class ManagerRadius {
   static double r45 = SizeUtil.setRadius(45);
+  static double r20 = SizeUtil.setRadius(20);
+  static double r10 = SizeUtil.setRadius(10);
+  static double r5 = SizeUtil.setRadius(5);
 }
+class ManagerIconsSizes {
+  static double i18 = SizeUtil.setIconSize(18);
+  static double i24 = SizeUtil.setIconSize(24);
+}
+
