7c00e0ec1ca5837d10ee91300a806bcffeb74e77
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-23 21:14:30 +0200

    add: implement route middleware and update route generator for login screen BLoC integration

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wafir_mobile/config/dependency_injection.dart';
 import 'package:wafir_mobile/core/resource/manager_colors.dart';
 import 'package:wafir_mobile/core/resource/manager_fonts.dart';
 import 'package:wafir_mobile/core/resource/manager_strings.dart';
+import 'package:wafir_mobile/features/login/presentation/controller/login_bloc.dart';
 import 'package:wafir_mobile/features/login/presentation/view/screens/login_screen.dart';
 import 'package:wafir_mobile/features/on_boarding/presentation/view/screens/on_boarding_screen.dart';
 import 'package:wafir_mobile/routes/routes.dart';
@@ -10,7 +13,13 @@ class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings setting) {
     switch (setting.name) {
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        initLogin();
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider<LoginBloc>(
+            create: (context) => instance<LoginBloc>(),
+            child: LoginScreen(),
+          ),
+        );
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       default:
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,4 +2,5 @@ class Routes {
   static const String onBoardingScreen = '/on_boarding_screen';
   static const String loginScreen = '/login_screen';
   static const String homeScreen = '/home_screen';
+  static const String forgetPasswordScreen = '/forget_password_screen';
 }
