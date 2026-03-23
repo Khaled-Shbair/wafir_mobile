ec95f6c4261461a2fe4a32ae229c3f03f7c2f0e7
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-23 21:13:14 +0200

    add: implement custom toast message utility for improved user notifications

---
+++ lib/core/widgets/custom_toast_massage.dart
@@ -0,0 +1,15 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:wafir_mobile/core/resource/manager_colors.dart';
+
+mixin CustomToastMassage {
+  void showToast(String message, [bool isError = true]) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor:
+            isError ? ManagerColors.failureColor : ManagerColors.secondaryColor,
+        fontSize: 16.0);
+  }
+}
