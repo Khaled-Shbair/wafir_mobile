37c6c3bf3dacc4fe79bb3118a1a4d14a8e0cc5de
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-23 21:14:52 +0200

    add: add localized strings for login and registration flow

--- lib/core/resource/manager_strings.dart
+++ lib/core/resource/manager_strings.dart
@@ -13,4 +13,16 @@ class ManagerStrings {
   static String next = 'next'.tr();
   static String previous = 'previous'.tr();
   static String skip = 'skip'.tr();
+  static String welcomeBack = 'welcome_back'.tr();
+  static String loginToCompleteToAccount = 'login_to_complete_to_account'.tr();
+  static String email = 'email'.tr();
+  static String enterEmail = 'enter_email'.tr();
+  static String password = 'password'.tr();
+  static String enterPassword = 'enter_password'.tr();
+  static String doNotHaveAnAccount = 'do_not_have_an_account'.tr();
+  static String register = 'register'.tr();
+  static String orLoginBy = 'or_login_by'.tr();
+  static String google = 'google'.tr();
+  static String rememberMy = 'remember_my'.tr();
+  static String didYouForgetYourPassword = 'did_you_forget_your_password'.tr();
 }
