f6414d00010c167d0da49a8f22cab885b151a5fc
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-23 21:18:20 +0200

    add: implement login bloc registration and disposal in dependency injection

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -1,12 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wafir_mobile/core/storage/local/shared_preferences_controller.dart';
+import 'package:wafir_mobile/features/login/presentation/controller/login_bloc.dart';
 
 final instance = GetIt.instance;
 
 Future<void> initModule() async {
   await _initSharedPreferences();
 }
+
 Future<void> _initSharedPreferences() async {
   if (!GetIt.I.isRegistered<SharedPreferences>()) {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
@@ -14,6 +16,18 @@ Future<void> _initSharedPreferences() async {
   }
   if (!GetIt.I.isRegistered<SharedPreferencesController>()) {
     instance.registerLazySingleton(
-            () => SharedPreferencesController(instance<SharedPreferences>()));
+        () => SharedPreferencesController(instance<SharedPreferences>()));
+  }
+}
+
+void initLogin() async {
+  if (!GetIt.I.isRegistered<LoginBloc>()) {
+    instance.registerLazySingleton<LoginBloc>(() => LoginBloc());
+  }
+}
+
+void disposeLogin() async {
+  if (GetIt.I.isRegistered<LoginBloc>()) {
+    instance.unregister<LoginBloc>();
   }
 }
