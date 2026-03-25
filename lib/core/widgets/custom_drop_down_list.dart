de326bee9e4715b3e42c27c935f180f1c826634a
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-25 18:45:06 +0200

    add: implement CustomDropDownList widget for enhanced dropdown functionality

---
+++ lib/core/widgets/custom_drop_down_list.dart
@@ -0,0 +1,97 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:flutter/material.dart';
+import 'package:wafir_mobile/core/resource/manager_sizes.dart';
+
+class CustomDropDownList extends StatelessWidget {
+  const CustomDropDownList({
+    required this.selectedItem,
+    required this.items,
+    required this.onChangedFunction,
+    this.labelText,
+    this.hintText,
+    this.labelStyle,
+    this.validator,
+    this.enabled = true,
+    super.key,
+  });
+
+  final String? selectedItem;
+  final String? labelText;
+  final String? hintText;
+  final List<String> items;
+  final Function(String? v) onChangedFunction;
+  final String? Function(String?)? validator;
+  final TextStyle? labelStyle;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownSearch<String>(
+      validator: validator,
+      enabled: enabled,
+      autoValidateMode: AutovalidateMode.onUnfocus,
+      items: (filter, loadProps) => items,
+      itemAsString: (item) => item,
+      compareFn: (item, selectedItem) => item == selectedItem,
+      selectedItem: selectedItem,
+      onChanged: (String? selectedReason) => onChangedFunction(selectedReason),
+      suffixProps: DropdownSuffixProps(
+        dropdownButtonProps: DropdownButtonProps(
+          splashColor: Theme.of(context).disabledColor,
+          padding: EdgeInsetsDirectional.zero,
+          iconClosed: Icon(Icons.keyboard_arrow_down_sharp,
+              color: Theme.of(context).colorScheme.primary),
+          iconOpened: Icon(Icons.keyboard_arrow_up_sharp,
+              color: Theme.of(context).colorScheme.primary),
+          iconSize: ManagerIconsSizes.i24,
+        ),
+      ),
+      popupProps: PopupProps.menu(
+        itemBuilder: (context, item, isDisabled, isSelected) {
+          return Container(
+            height: ManagerHeights.h30,
+            alignment: AlignmentDirectional.centerStart,
+            padding: EdgeInsetsDirectional.only(
+              start: ManagerWidths.w10,
+              end: ManagerWidths.w10,
+            ),
+            child: Text(
+              item,
+              textAlign: TextAlign.start,
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+          );
+        },
+        fit: FlexFit.loose,
+        menuProps: MenuProps(
+          align: MenuAlign.bottomCenter,
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          margin: EdgeInsets.only(
+            bottom: ManagerHeights.h20,
+          ),
+        ),
+        cacheItems: true,
+      ),
+      dropdownBuilder: (context, selectedItem) {
+        return Text(
+          selectedItem ?? '',
+          style: Theme.of(context).textTheme.labelLarge,
+        );
+      },
+      decoratorProps: DropDownDecoratorProps(
+        textAlign: TextAlign.center,
+        decoration: InputDecoration(
+          disabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade400),
+            borderRadius: BorderRadius.circular(ManagerRadius.r10),
+          ),
+          labelText: labelText,
+          isDense: true,
+          hintText: hintText,
+          hintStyle: labelStyle,
+          labelStyle: labelStyle,
+        ),
+      ),
+    );
+  }
+}
