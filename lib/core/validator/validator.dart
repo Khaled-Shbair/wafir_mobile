766660195ff1fa47f26c899ff8177f4a2c5e0a7c
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-25 18:43:17 +0200

    add: implement name and phone number validation methods

--- lib/core/validator/validator.dart
+++ lib/core/validator/validator.dart
@@ -1,6 +1,26 @@
 class Validator {
-  static String?  emailValidator(String? email) {
-}
+  static String? emailValidator(String? email) {}
+
+  static String? nameValidate(String? name) {
+    if (name!.isEmpty) {
+      return 'Invalid Name';
+    }
+    if (!RegExp(r'^[a-zA-Z]+$').hasMatch(name)) {
+      return 'Name must contain only letters.';
+    }
+    return null;
+  }
+
+  static String? phoneValidate(String? phone) {
+    if (phone!.isEmpty) {
+      return 'Invalid Phone Number';
+    }
+    if (!RegExp(r'^\d{10}$').hasMatch(phone)) {
+      return 'Phone number must be 10 digits long.';
+    }
+    return null;
+  }
+
   static String? passwordValidator(String? password) {
     if (password!.isEmpty) {
       return 'Invalid Password';
