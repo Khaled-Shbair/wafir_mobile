cfb19e26f2b7df01b846d8345781ac38d7973900
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-26 23:38:41 +0200

    add: update white color and add grey and disabled colors to ManagerColors

--- lib/core/resource/manager_colors.dart
+++ lib/core/resource/manager_colors.dart
@@ -4,9 +4,11 @@ class ManagerColors {
   static const Color primaryColor = Color(0xff00845E);
   static const Color secondaryColor = Color(0xfffa7d22);
   static const Color blackColor = Color(0xff010e0a);
-  static const Color whiteColor = Color(0xfffffff2);
+  static const Color whiteColor = Color(0xffffffff);
   static Color shadowColor = blackColor.withOpacity(0.25);
   static Color transparentColor = Colors.transparent;
   static Color successfullyColor = Colors.green;
   static Color  failureColor = Colors.red;
+  static Color  disabledColor = Colors.grey.shade400;
+  static Color  greyColor = Colors.grey;
 }
