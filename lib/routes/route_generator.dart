3110f082451ddfd23ae1d5c71fe8faaa9171b830
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-26 23:41:18 +0200

    add: define additional width and height constants in ManagerSizes

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -4,11 +4,16 @@ import 'package:wafir_mobile/config/dependency_injection.dart';
 import 'package:wafir_mobile/core/resource/manager_colors.dart';
 import 'package:wafir_mobile/core/resource/manager_fonts.dart';
 import 'package:wafir_mobile/core/resource/manager_strings.dart';
+import 'package:wafir_mobile/features/change_password/presentation/controller/change_password_bloc.dart';
+import 'package:wafir_mobile/features/change_password/presentation/view/change_password_screen.dart';
 import 'package:wafir_mobile/features/create_account/presentation/controller/create_account_bloc.dart';
 import 'package:wafir_mobile/features/create_account/presentation/screens/create_account_screen.dart';
 import 'package:wafir_mobile/features/login/presentation/controller/login_bloc.dart';
 import 'package:wafir_mobile/features/login/presentation/view/screens/login_screen.dart';
 import 'package:wafir_mobile/features/on_boarding/presentation/view/screens/on_boarding_screen.dart';
+import 'package:wafir_mobile/features/forget_password/presentation/controller/forget_password_bloc.dart';
+import 'package:wafir_mobile/features/forget_password/presentation/view/forget_password_screen.dart';
+import 'package:wafir_mobile/features/setting/presentation/view/screens/setting_screen.dart';
 import 'package:wafir_mobile/routes/routes.dart';
 
 class RouteGenerator {
@@ -32,6 +37,26 @@ class RouteGenerator {
         );
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case Routes.changePasswordScreen:
+        initChangePassword();
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider<ChangePasswordBloc>(
+            create: (context) => instance<ChangePasswordBloc>(),
+            child: const ChangePasswordScreen(),
+          ),
+        );
+      case Routes.forgetPasswordScreen:
+        initForgetPassword();
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider<ForgetPasswordBloc>(
+            create: (context) => instance<ForgetPasswordBloc>(),
+            child: const ForgetPasswordScreen(),
+          ),
+        );
+      case Routes.settingScreen:
+        return MaterialPageRoute(
+          builder: (_) => SettingScreen(),
+        );
       default:
         return unDefinedRoute();
     }
