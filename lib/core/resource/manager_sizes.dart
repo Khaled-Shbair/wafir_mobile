db225e68eef46c23b10a48927b9535c3ee6503e7
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-26 23:41:07 +0200

    add: define additional width and height constants in ManagerSizes

--- lib/core/resource/manager_sizes.dart
+++ lib/core/resource/manager_sizes.dart
@@ -3,19 +3,29 @@ import 'package:wafir_mobile/core/size_util/size_util.dart';
 class ManagerWidths {
   static const double infinity = double.infinity;
   static  double w10 = SizeUtil.setWidth(10);
+  static  double w180 = SizeUtil.setWidth(180);
   static  double w20 = SizeUtil.setWidth(20);
+  static  double w35 = SizeUtil.setWidth(35);
   static  double w5 = SizeUtil.setWidth(5);
+  static  double w120 = SizeUtil.setWidth(120);
 
 }
 
 class ManagerHeights {
   static const double infinity = double.infinity;
+  static double h5 = SizeUtil.setHeight(5);
   static double h7 = SizeUtil.setHeight(7);
   static double h10 = SizeUtil.setHeight(10);
+  static double h15 = SizeUtil.setHeight(15);
+  static double h35 = SizeUtil.setHeight(35);
   static double h50 = SizeUtil.setHeight(50);
+  static double h70 = SizeUtil.setHeight(70);
   static double h20 = SizeUtil.setHeight(20);
   static double h30 = SizeUtil.setHeight(30);
   static double h40 = SizeUtil.setHeight(40);
+  static double h120 = SizeUtil.setHeight(120);
+  static double h300 = SizeUtil.setHeight(300);
+  static double h320 = SizeUtil.setHeight(320);
   static double h350 = SizeUtil.setHeight(350);
 
 }
