e5b237c489d12fcc05035a31b61c024bc5a00360
Khaled Shbair <khaled.shbair12@gmail.com>
2026-03-31 14:42:37 +0300

    add: extend manager_sizes with additional width and height constants

--- lib/core/resource/manager_sizes.dart
+++ lib/core/resource/manager_sizes.dart
@@ -7,7 +7,10 @@ class ManagerWidths {
   static  double w20 = SizeUtil.setWidth(20);
   static  double w35 = SizeUtil.setWidth(35);
   static  double w5 = SizeUtil.setWidth(5);
+  static  double w15 = SizeUtil.setWidth(15);
+  static  double w25 = SizeUtil.setWidth(25);
   static  double w120 = SizeUtil.setWidth(120);
+  static  double w100 = SizeUtil.setWidth(100);
 
 }
 
@@ -17,12 +20,15 @@ class ManagerHeights {
   static double h7 = SizeUtil.setHeight(7);
   static double h10 = SizeUtil.setHeight(10);
   static double h15 = SizeUtil.setHeight(15);
+  static double h18 = SizeUtil.setHeight(18);
+  static double h25 = SizeUtil.setHeight(25);
   static double h35 = SizeUtil.setHeight(35);
   static double h50 = SizeUtil.setHeight(50);
   static double h70 = SizeUtil.setHeight(70);
   static double h20 = SizeUtil.setHeight(20);
   static double h30 = SizeUtil.setHeight(30);
   static double h40 = SizeUtil.setHeight(40);
+  static double h100 = SizeUtil.setHeight(100);
   static double h120 = SizeUtil.setHeight(120);
   static double h140 = SizeUtil.setHeight(140);
   static double h300 = SizeUtil.setHeight(300);
