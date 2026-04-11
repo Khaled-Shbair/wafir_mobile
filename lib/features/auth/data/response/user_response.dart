816d875f2ee03fae047b56ef91b441bed853cdda
Khaled Shbair <khaled.shbair12@gmail.com>
2026-04-11 18:31:53 +0300

    add: create UserModel and UserResponse classes with JSON serialization

---
+++ lib/features/auth/data/response/user_response.dart
@@ -0,0 +1,58 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:wafir_mobile/config/constants/api_constants.dart';
+
+part 'user_response.g.dart';
+
+@JsonSerializable()
+class UserResponse {
+  @JsonKey(name: ApiKeys.id)
+  final int? id;
+  @JsonKey(name: ApiKeys.firstName)
+  final String? firstName;
+  @JsonKey(name: ApiKeys.lastName)
+  final String? lastName;
+  @JsonKey(name: ApiKeys.phoneNumber)
+  final String? phoneNumber;
+  @JsonKey(name: ApiKeys.email)
+  final String? email;
+  @JsonKey(name: ApiKeys.role)
+  final String? role;
+  @JsonKey(name: ApiKeys.governorate)
+  final String? governorate;
+  @JsonKey(name: ApiKeys.wilaya)
+  final String? wilaya;
+  @JsonKey(name: ApiKeys.status)
+  final String? status;
+  @JsonKey(name: ApiKeys.fcmToken)
+  final String? fcmToken;
+  @JsonKey(name: ApiKeys.avatarUrl)
+  final String? avatarUrl;
+  @JsonKey(name: ApiKeys.avatarPublicId)
+  final String? avatarPublicId;
+  @JsonKey(name: ApiKeys.createdAt)
+  final String? createdAt;
+  @JsonKey(name: ApiKeys.updatedAt)
+  final String? updatedAt;
+
+  UserResponse({
+    this.id,
+    this.firstName,
+    this.lastName,
+    this.phoneNumber,
+    this.email,
+    this.role,
+    this.governorate,
+    this.wilaya,
+    this.status,
+    this.fcmToken,
+    this.avatarUrl,
+    this.avatarPublicId,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory UserResponse.fromJson(Map<String, dynamic> json) =>
+      _$UserResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserResponseToJson(this);
+}
---
+++ lib/features/auth/domain/model/user_model.dart
@@ -0,0 +1,33 @@
+class UserModel {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String phoneNumber;
+  final String email;
+  final String role;
+  final String governorate;
+  final String wilaya;
+  final String status;
+  final String fcmToken;
+  final String avatarUrl;
+  final String avatarPublicId;
+  final String createdAt;
+  final String updatedAt;
+
+  UserModel({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.phoneNumber,
+    required this.email,
+    required this.role,
+    required this.governorate,
+    required this.wilaya,
+    required this.status,
+    required this.fcmToken,
+    required this.avatarUrl,
+    required this.avatarPublicId,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+}
