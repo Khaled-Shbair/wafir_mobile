df5eb5b6dd33e108a89ba6b3e3c8a0b1337b1a74
Khaled Shbair <khaled.shbair12@gmail.com>
2026-04-11 18:27:47 +0300

    add: implement base use case classes for input and output handling

---
+++ lib/core/use_case/base_use_case.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:wafir_mobile/core/error_handler/error_handler.dart';
+
+abstract class BaseUseCase<In, Out> {
+  Future<Either<Failure, Out>> execute(In input);
+}
+abstract class BaseOutUseCase<Out> {
+  Future<Either<Failure, Out>> execute();
+}
