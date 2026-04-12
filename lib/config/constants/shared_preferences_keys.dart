35d9583c6b59639abf26aa7b7b754ec12f54d7fb
Khaled Shbair <khaled.shbair12@gmail.com>
2026-04-12 19:23:07 +0300

    add: add token key to shared preferences for authentication management

--- lib/config/constants/shared_preferences_keys.dart
+++ lib/config/constants/shared_preferences_keys.dart
@@ -2,4 +2,5 @@ class SharedPreferencesKeys {
   static const String viewOnBoarding = 'view_on_boarding';
   static const String language = 'language';
   static const String userData = 'user_data';
+  static const String token = 'token';
 }
