3d178d128eba84d7402e413361690828dc82a6e5
Khaled Shbair <khaled.shbair12@gmail.com>
2026-04-15 09:04:26 +0300

    add: implement toggle favorite offer model and data mapper

---
+++ lib/features/favorite/data/mapper/toggle_favorite_offer_mapper.dart
@@ -0,0 +1,12 @@
+import 'package:wafir_mobile/core/extensions/extensions.dart';
+import 'package:wafir_mobile/features/favorite/data/response/favorite_toggle_response.dart';
+import 'package:wafir_mobile/features/favorite/domain/model/toggle_favorite_offer_model.dart';
+
+extension ToggleFavoriteOfferMapper on FavoriteToggleResponse {
+  ToggleFavoriteOfferModel toDomain() {
+    return ToggleFavoriteOfferModel(
+      message: message.onNull(),
+      success: success.onNull(),
+    );
+  }
+}
---
+++ lib/features/favorite/domain/model/toggle_favorite_offer_model.dart
@@ -0,0 +1,9 @@
+class ToggleFavoriteOfferModel {
+  final bool success;
+  final String message;
+
+  ToggleFavoriteOfferModel({
+    required this.message,
+    required this.success,
+  });
+}
