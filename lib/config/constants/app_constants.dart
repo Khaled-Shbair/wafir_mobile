0311a1afac388bba7a4162f394bb8d96c4780274
Khaled Shbair <khaled.shbair12@gmail.com>
2026-04-15 08:58:49 +0300

    add: implement getRemainingDays helper for Arabic date formatting in AppConstants

--- lib/config/constants/app_constants.dart
+++ lib/config/constants/app_constants.dart
@@ -1,4 +1,22 @@
 class AppConstants{
+ static String getRemainingDays(String validUntil) {
+    final now = DateTime.now();
+    final endDate = DateTime.parse(validUntil);
+
+    final difference = endDate.difference(now).inDays;
+
+    if (difference <= 0) {
+      return "انتهى";
+    } else if (difference == 1) {
+      return "يوم واحد";
+    } else if (difference == 2) {
+      return "يومين";
+    } else if (difference <= 10) {
+      return "$difference أيام";
+    } else {
+      return "$difference يوم";
+    }
+  }
   static const double deviceHeight = 812;
   static const double deviceWidth = 375;
   static final Map<String, List<String>> omanLocations = {
