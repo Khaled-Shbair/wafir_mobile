b1e03270b42c4f6bfcd8e81034efcb56c39ec8f4
Khaled Shbair <khaled.shbair12@gmail.com>
2026-04-30 21:42:33 +0300

    refactor: add WelcomeScreen with animated image transition and navigation options

--- lib/core/resource/manager_assets.dart
+++ lib/core/resource/manager_assets.dart
@@ -1,13 +1,19 @@
-class ManagerAssets{
+class ManagerAssets {
   static const String splashGif = 'assets/images/splash/wafer_logo.gif';
   static const String appLogo = 'assets/images/splash/app_logo.png';
   static const String logo = 'assets/images/splash/wafer_logo.png';
+  static const String welcomeImageGif = 'assets/images/login_images/welcome_image.gif';
+  static const String welcomeImage = 'assets/images/login_images/welcome_image.jpeg';
 
-  static const String onBoardingImageOne = 'assets/images/on_boarding_images/on_boarding_image_one.png';
-  static const String onBoardingImageTwo = 'assets/images/on_boarding_images/on_boarding_image_two.png';
-  static const String onBoardingImageThree = 'assets/images/on_boarding_images/on_boarding_image_three.png';
+  static const String onBoardingImageOne =
+      'assets/images/on_boarding_images/on_boarding_image_one.png';
+  static const String onBoardingImageTwo =
+      'assets/images/on_boarding_images/on_boarding_image_two.png';
+  static const String onBoardingImageThree =
+      'assets/images/on_boarding_images/on_boarding_image_three.png';
   static const String googleIcon = 'assets/images/login_images/google_Icon.png';
-  static const String personImage = 'assets/images/profile_images/profile_erorr_image.jpeg';
+  static const String personImage =
+      'assets/images/profile_images/profile_erorr_image.jpeg';
   static const String lockIcon = 'assets/images/icons/lock.png';
   static const String favoriteIcon = 'assets/images/icons/favorite.png';
   static const String logoutIcon = 'assets/images/icons/logout.png';
---
+++ lib/features/auth/presentation/view/screens/welcome_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:wafir_mobile/core/resource/manager_assets.dart';
+import 'package:wafir_mobile/core/resource/manager_colors.dart';
+import 'package:wafir_mobile/core/resource/manager_sizes.dart';
+import 'package:wafir_mobile/core/resource/manager_strings.dart';
+import 'package:wafir_mobile/core/widgets/custom_button.dart';
+import 'package:wafir_mobile/core/widgets/custom_spacing.dart';
+import 'package:wafir_mobile/routes/routes.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  bool finished = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 4), () {
+      setState(() => finished = true);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ManagerColors.primaryColor,
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Stack(
+            children: [
+              Positioned.fill(
+                child: AnimatedOpacity(
+                  opacity: finished ? 0 : 1,
+                  duration: Duration.zero, // ❗ بدون أنيميشن
+                  child: Image.asset(
+                    ManagerAssets.welcomeImageGif,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: AnimatedOpacity(
+                  opacity: finished ? 1 : 0,
+                  duration: Duration.zero,
+                  child: Image.asset(
+                    ManagerAssets.welcomeImage,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.only(
+              start: ManagerWidths.w20,
+              end: ManagerWidths.w20,
+              bottom: ManagerHeights.h45,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(Routes.loginScreen);
+                    },
+                    text: ManagerStrings.login,
+                    color: ManagerColors.buttonColor,
+                    colorText: ManagerColors.whiteColor,
+                  ),
+                ),
+                horizontalSpace(ManagerWidths.w15),
+                Expanded(
+                  child: CustomButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(
+                          Routes.mainScreen,);
+                    },
+                    text: ManagerStrings.loginAsGuest,
+                    color: ManagerColors.whiteColor,
+                    colorText: ManagerColors.buttonColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
