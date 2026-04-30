3031fa118747d9bf42e2f2bf8a7b3990aff9a564
Khaled Shbair <khaled.shbair12@gmail.com>
2026-04-30 21:40:48 +0300

    refactor: convert LoginScreen to StatefulWidget and enhance input handling

--- lib/features/auth/presentation/view/screens/login_screen.dart
+++ lib/features/auth/presentation/view/screens/login_screen.dart
@@ -1,13 +1,12 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:wafir_mobile/config/constants/shared_preferences_keys.dart';
 import 'package:wafir_mobile/config/dependency_injection.dart';
 import 'package:wafir_mobile/core/resource/manager_assets.dart';
 import 'package:wafir_mobile/core/resource/manager_colors.dart';
 import 'package:wafir_mobile/core/resource/manager_fonts.dart';
 import 'package:wafir_mobile/core/resource/manager_sizes.dart';
 import 'package:wafir_mobile/core/resource/manager_strings.dart';
-import 'package:wafir_mobile/core/storage/local/shared_preferences_controller.dart';
 import 'package:wafir_mobile/core/validator/validator.dart';
 import 'package:wafir_mobile/core/widgets/custom_button.dart';
 import 'package:wafir_mobile/core/widgets/custom_loading.dart';
@@ -18,15 +17,54 @@ import 'package:wafir_mobile/features/auth/presentation/view/widgets/custom_acti
 import 'package:wafir_mobile/routes/routes.dart';
 import 'package:wafir_mobile/features/auth/presentation/controller/login_bloc.dart';
 
-class LoginScreen extends StatelessWidget with CustomToastMassage {
+class LoginScreen extends StatefulWidget with CustomToastMassage {
   LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> with CustomToastMassage {
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passwordFocusNode = FocusNode();
+  final TapGestureRecognizer createAccount = TapGestureRecognizer();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  bool emailIsFocused = false;
+  bool passwordIsFocused = false;
+
+  @override
+  initState() {
+    super.initState();
+    emailFocusNode.addListener(() {
+      setState(() {
+        emailIsFocused = emailFocusNode.hasFocus;
+      });
+    });
+    passwordFocusNode.addListener(() {
+      setState(() {
+        passwordIsFocused = passwordFocusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  dispose() {
+    email.dispose();
+    password.dispose();
+    createAccount.dispose();
+    emailFocusNode.dispose();
+    passwordFocusNode.dispose();
+    disposeLogin();
+    disposeAuth();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     var controller = context.read<LoginBloc>();
     return BlocListener<LoginBloc, LoginState>(
       listener: (_, state) {
-
         if (state is LoginLoading) {
           showCustomLoading(context);
         } else if (state is LoginSuccessfully) {
@@ -36,198 +74,204 @@ class LoginScreen extends StatelessWidget with CustomToastMassage {
           Navigator.pushNamedAndRemoveUntil(
               context, Routes.mainScreen, (route) => false);
         } else if (state is LoginFailure) {
+        } else if (state is LoginFailure) {
           // Close loading dialog using root navigator
           Navigator.of(context, rootNavigator: true).pop();
           if (state.errorMessage.contains('تحقق')) {
-            customActivationMessenger(context, controller.email.text);
+            customActivationMessenger(context, email.text);
           } else {
             showToast(state.errorMessage);
           }
         }
-        print('Token :${instance<SharedPreferencesController>()
-            .getString(SharedPreferencesKeys.token)}');
       },
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.grey.shade200,
-        body: SafeArea(child: Container(
-          height: ManagerHeights.infinity,
-          padding: EdgeInsetsDirectional.only(
-            top: ManagerHeights.h30,
-            bottom: ManagerHeights.h30,
-            start: ManagerWidths.w20,
-            end: ManagerWidths.w20,
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(
+              Icons.arrow_back_ios_new_outlined,
+              size: ManagerIconsSizes.i20,
+            ),
           ),
-          margin: EdgeInsetsDirectional.only(
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsetsDirectional.only(
             top: ManagerHeights.h40,
             bottom: ManagerHeights.h40,
-            start: ManagerWidths.w20,
-            end: ManagerWidths.w20,
+            start: ManagerWidths.w25,
+            end: ManagerWidths.w25,
           ),
-          decoration: BoxDecoration(
-            color: ManagerColors.whiteColor,
-            borderRadius: BorderRadius.circular(ManagerRadius.r10),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Text(
-                  ManagerStrings.welcomeBack,
-                  style: Theme.of(context).textTheme.headlineLarge,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                ManagerStrings.welcomeBack,
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              verticalSpace(ManagerHeights.h10),
+              Text(ManagerStrings.loginToCompleteToAccount,
+                  style: Theme.of(context).textTheme.labelMedium),
+              verticalSpace(ManagerHeights.h50),
+              Form(
+                key: controller.formKey,
+                child: Column(
+                  children: [
+                    CustomTextField(
+                      maxLength: 30,
+                      prefixIconData: Icons.email_outlined,
+                      keyboardType: TextInputType.emailAddress,
+                      hintText: ManagerStrings.email,
+                      isFocused: emailIsFocused,
+                      textInputAction: TextInputAction.next,
+                      controller: email,
+                      focusNode: emailFocusNode,
+                      validator: (v) => Validator.emailValidator(v),
+                    ),
+                    verticalSpace(ManagerHeights.h20),
+                    BlocBuilder<LoginBloc, LoginState>(
+                      builder: (context, state) {
+                        return CustomTextField(
+                          maxLength: 50,
+                          focusNode: passwordFocusNode,
+                          textInputAction: TextInputAction.done,
+                          prefixIconData: Icons.lock_outline,
+                          isFocused: passwordIsFocused,
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: state.passwordVisible,
+                          isPassword: true,
+                          hintText: ManagerStrings.password,
+                          controller: password,
+                          validator: (v) => Validator.passwordValidator(v),
+                          functionVisibilityPassword: () =>
+                              controller.add(TogglePasswordVisibility()),
+                        );
+                      },
+                    ),
+                  ],
                 ),
-                verticalSpace(ManagerHeights.h10),
-                Text(ManagerStrings.loginToCompleteToAccount,
-                    style: Theme.of(context).textTheme.labelMedium),
-                verticalSpace(ManagerHeights.h50),
-                Form(
-                  key: controller.formKey,
-                  child: Column(
+              ),
+              verticalSpace(ManagerHeights.h20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      CustomTextField(
-                        maxLength: 30,
-                        prefixIcon: Icon(
-                          Icons.email_outlined,
-                          size: ManagerIconsSizes.i24,
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        hintText: ManagerStrings.email,
-                        textInputAction: TextInputAction.next,
-                        controller: controller.email,
-                        validator: (v) => Validator.emailValidator(v),
-                      ),
-                      verticalSpace(ManagerHeights.h20),
                       BlocBuilder<LoginBloc, LoginState>(
                         builder: (context, state) {
-                          return CustomTextField(
-                            maxLength: 50,
-                            textInputAction: TextInputAction.done,
-                            prefixIcon: Icon(
-                              Icons.lock_outline,
-                              size: ManagerIconsSizes.i24,
-                            ),
-                            keyboardType: TextInputType.visiblePassword,
-                            obscureText: state.passwordVisible,
-                            isPassword: true,
-                            hintText: ManagerStrings.password,
-                            controller: controller.password,
-                            validator: (v) => Validator.passwordValidator(v),
-                            functionVisibilityPassword: () =>
-                                controller.add(TogglePasswordVisibility()),
+                          return Checkbox(
+                            materialTapTargetSize:
+                                MaterialTapTargetSize.shrinkWrap,
+                            splashRadius: 0,
+                            value: state.rememberMe,
+                            onChanged: (value) => controller
+                                .add(ToggleRememberMe(value ?? false)),
                           );
                         },
                       ),
-                    ],
-                  ),
-                ),
-                verticalSpace(ManagerHeights.h20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        BlocBuilder<LoginBloc, LoginState>(
-                          builder: (context, state) {
-                            return Checkbox(
-                              materialTapTargetSize:
-                              MaterialTapTargetSize.shrinkWrap,
-                              splashRadius: 0,
-                              value: state.rememberMe,
-                              onChanged: (value) => controller
-                                  .add(ToggleRememberMe(value ?? false)),
-                            );
-                          },
-                        ),
-                        Text(
-                          ManagerStrings.rememberMy,
-                          style: Theme.of(context).textTheme.labelLarge,
-                        ),
-                      ],
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                        ScaffoldMessenger.of(context).clearSnackBars();
-                        Navigator.pushNamed(
-                            context, Routes.forgetPasswordScreen);
-                      },
-                      child: Text(
-                        ManagerStrings.didYouForgetYourPassword,
+                      Text(
+                        ManagerStrings.rememberMy,
                         style: Theme.of(context).textTheme.labelLarge,
                       ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                      ScaffoldMessenger.of(context).clearSnackBars();
+                      Navigator.pushNamed(context, Routes.forgetPasswordScreen);
+                    },
+                    child: Text(
+                      ManagerStrings.didYouForgetYourPassword,
+                      style: Theme.of(context).textTheme.labelLarge,
                     ),
-                  ],
-                ),
-                verticalSpace(ManagerHeights.h20),
-                CustomButton(
-                  text: ManagerStrings.login,
-                  onPressed: () {
-                    FocusManager.instance.primaryFocus?.unfocus();
-                    ScaffoldMessenger.of(context).clearSnackBars();
-                    controller.add(LoginProcess());
-                  },
-                ),
-                verticalSpace(ManagerHeights.h40),
-                Row(
+                  ),
+                ],
+              ),
+              verticalSpace(ManagerHeights.h20),
+              CustomButton(
+                text: ManagerStrings.login,
+                onPressed: () {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  controller.add(LoginProcess(
+                    email: email.text,
+                    password: password.text,
+                  ));
+                },
+              ),
+              verticalSpace(ManagerHeights.h40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      indent: ManagerWidths.w5,
+                      color: ManagerColors.unFocusBorderColor,
+                    ),
+                  ),
+                  Text(
+                    ManagerStrings.orLoginBy,
+                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                          fontSize: ManagerFontsSizes.f14,
+                          color: ManagerColors.blackColor,
+                        ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      indent: ManagerWidths.w5,
+                      color: ManagerColors.unFocusBorderColor,
+                    ),
+                  ),
+                ],
+              ),
+              verticalSpace(ManagerHeights.h20),
+              CustomButton(
+                color: ManagerColors.unFocusBorderColor,
+
+                // borderColor: ManagerColors.transparentColor,
+                onPressed: () {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  ScaffoldMessenger.of(context).clearSnackBars();
+                  //TODO: implement login by google
+                  // controller.add(LoginByGoogle());
+                  showToast('سيتم تنفيذ تسجيل الدخول عبر جوجل قريباً');
+                },
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(child: Divider(endIndent: ManagerWidths.w5)),
-                    Text(
-                      ManagerStrings.orLoginBy,
-                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                        fontSize: ManagerFontsSizes.f14,
-                      ),
+                    Image.asset(
+                      ManagerAssets.googleIcon,
+                      width: ManagerWidths.w20,
                     ),
-                    Expanded(child: Divider(indent: ManagerWidths.w5)),
+                    horizontalSpace(ManagerWidths.w10),
+                    Text(ManagerStrings.google)
                   ],
                 ),
-                verticalSpace(ManagerHeights.h40),
-                CustomButton(
-                  color: ManagerColors.whiteColor,
-                  borderColor: ManagerColors.blackColor,
-                  onPressed: () {
-                    FocusManager.instance.primaryFocus?.unfocus();
-                    ScaffoldMessenger.of(context).clearSnackBars();
-                    //TODO: implement login by google
-                    // controller.add(LoginByGoogle());
-                    showToast('سيتم تنفيذ تسجيل الدخول عبر جوجل قريباً');
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        ManagerAssets.googleIcon,
-                        width: ManagerWidths.w20,
-                      ),
-                      horizontalSpace(ManagerWidths.w10),
-                      Text(ManagerStrings.google)
-                    ],
-                  ),
+              ),
+              verticalSpace(ManagerHeights.h50),
+              RichText(
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.labelLarge,
+                  text: ManagerStrings.doNotHaveAnAccount,
+                  children: [
+                    TextSpan(
+                      text: ' ${ManagerStrings.register}',
+                      style: Theme.of(context).textTheme.labelMedium,
+                      recognizer: createAccount
+                        ..onTap = () {
+                          ScaffoldMessenger.of(context).clearSnackBars();
+                          Navigator.pushNamed(context, Routes.registerScreen);
+                        },
+                    ),
+                  ],
                 ),
-                verticalSpace(ManagerHeights.h50),
-                RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.labelLarge,
-                    text: ManagerStrings.doNotHaveAnAccount,
-                    children: [
-                      TextSpan(
-                        text: ' ${ManagerStrings.register}',
-                        style: Theme.of(context).textTheme.labelMedium,
-                        recognizer: controller.createAccount
-                          ..onTap = () {
-                            ScaffoldMessenger.of(context).clearSnackBars();
-                            Navigator.pushNamed(context, Routes.registerScreen);
-                          },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ),),
+        ),
       ),
     );
   }
 }
-
